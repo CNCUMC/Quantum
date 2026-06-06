163dac33d9ee5a8b8aedf7912994fa67bc055e0a
Black_Moss <kytheminecraft@outlook.com>
2026-06-06 21:00:04 +0800

    w

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Antiquantum;
+
+[BepInPlugin(Guid, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    internal new static ManualLogSource Logger;
+    public const string Guid = "org.explosivehydra.antiquantum";
+    public const string Name = "Antiquantum";
+    public const string Version = "1.0.0";
+    private readonly Harmony _harmony = new(Guid);
+
+    public void Awake()
+    {
+        Logger = base.Logger;
+        _harmony.PatchAll();
+    }
+}
